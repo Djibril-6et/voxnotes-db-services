591344f6a6ce7dd81c7ed3730c5a55e3164a7e39
Saelyn <ben.fraj.fairouz@gmail.com>
2024-09-12 23:25:54 +0200

    addd example env + change user controller register route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-router.post('/', userController.createUser);
+router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
