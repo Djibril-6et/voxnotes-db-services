450ce3eea26a625dc7b235cdeee393a7c4a34e8f
Saelyn <ben.fraj.fairouz@gmail.com>
2024-09-17 23:04:29 +0200

    add upload and download file

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db.config');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.config");
 const cors = require("cors");
 
 dotenv.config();
-
 const app = express();
 
-// Database connection
-connectDB();
-
 // Middleware
-app.use(express.json());
-
 app.use(
   cors({
     origin: "*",
     methods: "GET,POST,PUT,DELETE",
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to the database
+connectDB();
 
 // Use routes
-app.use('/api', require('./routes/index.routes'));
+app.use("/api", require("./routes/index.routes"));
 
 const PORT = process.env.PORT || 9090;
 
