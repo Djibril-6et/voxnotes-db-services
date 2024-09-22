bfc9eed5a745d73ce6de470ff0e3ec4b62833c25
Hugo <hugo.marchao@ynov.com>
2024-09-22 12:18:51 +0200

    add transcriptionText to upload

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const userRoutes = require('./user.routes');
-const audioFileRoutes = require('./audioFile.routes');
-const transcriptionRoutes = require('./transcription.routes');
-const subscriptionRoutes = require('./subscription.routes');
+const express = require("express");
+const userRoutes = require("./user.routes");
+const audioFileRoutes = require("./audioFile.routes");
+const transcriptionRoutes = require("./transcription.routes");
+const subscriptionRoutes = require("./subscription.routes");
 
 const router = express.Router();
 
-router.use('/users', userRoutes);
-router.use('/audioFiles', audioFileRoutes);
-router.use('/transcriptions', transcriptionRoutes);
-router.use('/subscriptions', subscriptionRoutes);
+router.use("/users", userRoutes);
+router.use("/audioFiles", audioFileRoutes);
+router.use("/transcriptions", transcriptionRoutes);
+router.use("/subscriptions", subscriptionRoutes);
 
 module.exports = router;
